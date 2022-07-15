f53474ad43e0f3833b345c24b73fce91ab0f7455
WE4EVERKOMBAN <108579654+WE4EVERKOMBAN@users.noreply.github.com>
2022-07-15 21:29:25 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 module.exports = {
   token:'5466873914:AAFPbX5MCCw63hYOyAcgYT10uQhKwImdGjQ', // token from @BotFather
-  mongoLink: 'mongodb+srv://Visnu0p:1751@bittu.gv1tl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', // mongo-link from cloud.mongodb.com
+  mongoLink: 'mongodb+srv://Data1:Data1@cluster0.ekxpn.mongodb.net/?retryWrites=true&w=majority', // mongo-link from cloud.mongodb.com
   admin: '895687774', // admin`s id for sending him/her errors,
   cp_key:'bc9249663c991abdcf476789b549d1e78cbad9ce391bd307c971bfca05ff8072',
 0  cp_secret:'6491Bd1A7577A94A06a3776b8d2dF09Ec52056AAf2cb4Feb2A311257',
